0fecbbc8e13e0e24349fe7ae7adbd212612b4178
David <david.roquain@edu.esiee-it.fr>
2022-03-17 14:56:15 +0100

    pop

--- accueil.js
+++ accueil.js
@@ -6,23 +6,32 @@ fetch(requestURL)
     changetext(jsonData)
   })
 function changetext(site){
-    let Titre = document.getElementById('txtTitre');
-    let SousTitre = document.getElementById('stxtTitre');
-    let DescActi = document.getElementById('DescActi');
-    let Telephone = document.getElementById('Telephone');
-    let Mail = document.getElementById('Mail');
-    let Linkedin = document.getElementById('Linkedin');
-    let Local = document.getElementById('Local');
-    console.debug(DescActi.innerHTML);
-    Titre.innerHTML = site["Francais"]["Titre"];
-    SousTitre.innerHTML = site["Francais"]["SousTitre"];
-    DescActi.innerHTML = site["Francais"]["Activity"];
-    Telephone.innerHTML = site["Francais"]["Telephone"];
-    Telephone.href = site["Francais"]["TelephoneURL"];
-    Mail.innerHTML = site["Francais"]["Mail"];
-    Mail.href = site["Francais"]["MailURL"];
-    Linkedin.innerHTML = site["Francais"]["Linkedin"];
-    Linkedin.href = site["Francais"]["LinkedinURL"];
-    Local.innerHTML = site["Francais"]["Localisation"];
-    Local.href = site["Francais"]["LocalisationURL"];
+    document.getElementById('txtTitre').innerHTML = site["Francais"]["Titre"];
+    document.getElementById('stxtTitre').innerHTML = site["Francais"]["SousTitre"];
+    document.getElementById('DescActi').innerHTML = site["Francais"]["Activity"];
+    document.getElementById('Telephone').innerHTML = site["Francais"]["Telephone"];
+    document.getElementById('Telephone').href = site["Francais"]["TelephoneURL"];
+    document.getElementById('Mail').innerHTML = site["Francais"]["Mail"];
+    document.getElementById('Mail').href = site["Francais"]["MailURL"];
+    document.getElementById('Linkedin').innerHTML = site["Francais"]["Linkedin"];
+    document.getElementById('Linkedin').href = site["Francais"]["LinkedinURL"];
+    document.getElementById('Local').innerHTML = site["Francais"]["Localisation"];
+    document.getElementById('Local').href = site["Francais"]["LocalisationURL"];
+    document.getElementById('infoAccueil').innerHTML = '<img src="' + site["Francais"]["Backgroundimg"] + '" class="fondaccueil">' + document.getElementById('infoAccueil').innerHTML;
+    document.getElementById('L1').src = site["Francais"]["left1"];
+    document.getElementById('L2').src = site["Francais"]["left2"];
+    document.getElementById('L3').src = site["Francais"]["left3"];
+    document.getElementById('L4').src = site["Francais"]["left4"];
+    document.getElementById('R1').src = site["Francais"]["right1"];
+    document.getElementById('R2').src = site["Francais"]["right2"];
+    document.getElementById('R3').src = site["Francais"]["right3"];
+    document.getElementById('R4').src = site["Francais"]["right4"];
+    document.getElementById('M1').src = site["Francais"]["middle1"];
+    document.getElementById('M2').src = site["Francais"]["middle2"];
+    document.getElementById('M3').src = site["Francais"]["middle3"];
+    document.getElementById('M4').src = site["Francais"]["middle4"];
+    let Confiance = document.getElementById('Confiance')
+    for (let i = 0; i < site["Francais"]["Partenaire"].length; i++) {
+        Confiance.innerHTML = Confiance.innerHTML + '<div class="slide"><img src="' + site["Francais"]["Partenaire"][i]["Image"] +'"></div>';
+    }
 }
--- modif accueil.js
+++ modif accueil.js
@@ -6,22 +6,28 @@ fetch(requestURL)
     changetext(jsonData)
   })
 function changetext(site){
-  let Titre = document.getElementById('txtTitre');
-  let SousTitre = document.getElementById('stxtTitre');
-  let DescActi = document.getElementById('DescActi');
-  let Telephone = document.getElementById('Telephone');
-  let Mail = document.getElementById('Mail');
-  let Linkedin = document.getElementById('Linkedin');
-  let Local = document.getElementById('Local');
-  Titre.value = site["Francais"]["Titre"];
-  SousTitre.value = site["Francais"]["SousTitre"];
-  DescActi.value = site["Francais"]["Activity"];
-  Telephone.value = site["Francais"]["Telephone"];
-  TelephoneURL.value = site["Francais"]["TelephoneURL"];
-  Mail.value = site["Francais"]["Mail"];
-  MailURL.value = site["Francais"]["MailURL"];
-  Linkedin.value = site["Francais"]["Linkedin"];
-  LinkedinURL.value = site["Francais"]["LinkedinURL"];
-  Local.value = site["Francais"]["Localisation"];
-  LocalURL.value = site["Francais"]["LocalisationURL"];
+  document.getElementById('txtTitre').value = site["Francais"]["Titre"];
+  document.getElementById('stxtTitre').value = site["Francais"]["SousTitre"];
+  document.getElementById('DescActi').value = site["Francais"]["Activity"];
+  document.getElementById('Telephone').value = site["Francais"]["Telephone"];
+  document.getElementById('Telephone').value = site["Francais"]["TelephoneURL"];
+  document.getElementById('Mail').value = site["Francais"]["Mail"];
+  document.getElementById('Mail').value = site["Francais"]["MailURL"];
+  document.getElementById('Linkedin').value = site["Francais"]["Linkedin"];
+  document.getElementById('Linkedin').value = site["Francais"]["LinkedinURL"];
+  document.getElementById('Local').value = site["Francais"]["Localisation"];
+  document.getElementById('Local').value = site["Francais"]["LocalisationURL"];
+  document.getElementById('Background').value = site["Francais"]["Backgroundimg"];
+  document.getElementById('left1').value = site["Francais"]["left1"];
+  document.getElementById('left2').value = site["Francais"]["left2"];
+  document.getElementById('left3').value = site["Francais"]["left3"];
+  document.getElementById('left4').value = site["Francais"]["left4"];
+  document.getElementById('right1').value = site["Francais"]["right1"];
+  document.getElementById('right2').value = site["Francais"]["right2"];
+  document.getElementById('right3').value = site["Francais"]["right3"];
+  document.getElementById('right4').value = site["Francais"]["right4"];
+  document.getElementById('middle1').value = site["Francais"]["middle1"];
+  document.getElementById('middle2').value = site["Francais"]["middle2"];
+  document.getElementById('middle3').value = site["Francais"]["middle3"];
+  document.getElementById('middle4').value = site["Francais"]["middle4"];
 }
