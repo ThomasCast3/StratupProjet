bce84e517a016a455b3c778cb54b7061be3499b7
David <david.roquain@edu.esiee-it.fr>
2022-03-18 09:55:38 +0100

    Nombre de text area modifiable
    Prochaine étape stockage json

--- modif accueil.js
+++ modif accueil.js
@@ -30,12 +30,47 @@ function changetext(site){
   document.getElementById('middle2').value = site["Francais"]["middle2"];
   document.getElementById('middle3').value = site["Francais"]["middle3"];
   document.getElementById('middle4').value = site["Francais"]["middle4"];
+  function restart(Type){
+    if (Type == "Conf"){
+      for(let r = 0;r < site["Francais"]["Confiance"].length;r++){
+        document.getElementById('Conf'+ r).value = site["Francais"]["Confiance"][r]["Image"];
+      }
+    }
+    if (Type == "Part"){
+      for(let r = 0;r < site["Francais"]["Partenaire"].length;r++){
+        document.getElementById('Part'+ r).value = site["Francais"]["Partenaire"][r]["Image"];
+      }
+    }
+  }
   let Confiance = document.getElementById('Confiance')
-    for (let i = 0; i < site["Francais"]["Confiance"].length; i++) {
-        Confiance.innerHTML = Confiance.innerHTML + '<textarea name="Conf' + i + '" value="' + site["Francais"]["Confiance"][i]["Image"] +'"></textarea>';
+  let i = 0;
+    for (i = 0; i < site["Francais"]["Confiance"].length; i++) {
+      Confiance.innerHTML = Confiance.innerHTML + '<textarea id="Conf' + i + '" name="Conf' + i + '" ></textarea>';
     }
-    let Partenaire = document.getElementById('Partenaire')
-    for (let i = 0; i < site["Francais"]["Partenaire"].length; i++) {
-        Partenaire.innerHTML = Partenaire.innerHTML + '<textarea name="Part' + i + '" value="' + site["Francais"]["Partenaire"][i]["Image"] +'"></textarea>';
+    restart("Conf");
+    document.getElementById('ConfB+').addEventListener('click',function(){
+      Confiance.innerHTML = Confiance.innerHTML + '<textarea id="Conf' + i + '" name="Conf' + i + '" ></textarea>';
+      i++;
+      restart("Conf");
+    })
+    document.getElementById('ConfB-').addEventListener('click',function(){
+      i--;
+      document.getElementById('Conf'+i).remove();
+    })
+    let Partenaire = document.getElementById('Partenaire');
+    let j = 0;
+    for (j = 0; j < site["Francais"]["Partenaire"].length; j++) {
+      Partenaire.innerHTML = Partenaire.innerHTML + '<textarea id="Part' + j + '" name="Part' + j + '" ></textarea>';
+      document.getElementById('Part'+ j).value = site["Francais"]["Partenaire"][j]["Image"];
     }
+    restart("Part");
+    document.getElementById('PartB+').addEventListener('click',function(){
+      Partenaire.innerHTML = Partenaire.innerHTML + '<textarea id="Part' + j + '" name="Part' + j + '" ></textarea>';
+      j++;
+      restart("Part");
+    })
+    document.getElementById('PartB-').addEventListener('click',function(){
+      j--;
+      document.getElementById('Part'+j).remove();
+    })
 }
