6a5695dc04f71f7d49fe12201d00f66a033e5bf2
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2022-03-23 14:41:44 +0100

    reorganisation des fichiers

---
+++ test/fr/jsFR/scriptFR.js
@@ -0,0 +1,68 @@
+let carte1 = document.getElementById("carteSection1");
+let carte2 = document.getElementById("carteSection2");
+let carte3 = document.getElementById("carteSection3");
+let carte4 = document.getElementById("carteSection4");
+let carte5 = document.getElementById("carteSection5");
+
+let carte6 = document.getElementById("carteProduit1");
+let carte7 = document.getElementById("carteProduit2");
+let carte8 = document.getElementById("carteProduit3");
+let carte9 = document.getElementById("carteProduit4");
+let carte10 = document.getElementById("carteProduit5");
+let carte11 = document.getElementById("carteProduit6");
+let carte12 = document.getElementById("carteProduit7");
+let carte13 = document.getElementById("carteProduit8");
+
+carte1.addEventListener("click" , function() {
+location.href="serviceFR/service1.html";
+});
+
+carte2.addEventListener("click" , function() {
+location.href="serviceFR/service2.html";
+});
+
+carte3.addEventListener("click" , function() {
+location.href="serviceFR/service3.html";
+});
+
+carte4.addEventListener("click" , function() {
+location.href="serviceFR/service4.html";
+});
+
+carte5.addEventListener("click" , function() {
+location.href="serviceFR/service5.html";
+});
+
+
+
+carte6.addEventListener("click" , function() {
+location.href="ProduitFR/ToursDeLavageFR.html";
+});
+
+carte7.addEventListener("click" , function() {
+location.href="ProduitFR/VenturiFR.html";
+});
+
+carte8.addEventListener("click" , function() {
+location.href="ProduitFR/ElectrofiltresHumidesSecsFR.html";
+});
+
+carte9.addEventListener("click" , function() {
+location.href="ProduitFR/FiltresManchesFR.html";
+});
+
+carte10.addEventListener("click" , function() {
+location.href="ProduitFR/EquipementsDosageInjectionDePoudresFR.html";
+});
+
+carte11.addEventListener("click" , function() {
+    location.href="ProduitFR/InstallationsFR.html";
+    });
+
+carte12.addEventListener("click" , function() {
+    location.href="ProduitFR/RecuperationEnergiFR.html";
+});
+
+carte13.addEventListener("click" , function() {
+    location.href="ProduitFR/FourniturePiecesRechangeFR.html";
+    });
